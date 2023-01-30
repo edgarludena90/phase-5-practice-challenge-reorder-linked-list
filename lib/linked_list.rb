18fa9116599b866ae3683097c62ead1c22728924
edgar ludena <edgar.ludena90@gmail.com>
2023-01-30 07:59:30 -0500

    commit tree 2f00903709256ba46caef6042ab6cc67fb71ab62

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -8,6 +8,32 @@ class LinkedList
   end
 
   def reorder_linked_list
-    # your code here
+      # if the list is empty, no need to continue
+  return if head.nil?
+
+  # set odd to the first odd node and even to the first even node
+  odd = head
+  even = head.next_node
+
+  # keep a reference to the first even node
+  even_head = even
+
+  while even && even.next_node
+    # change odd's next_node property to point to the next odd node
+    odd.next_node = even.next_node
+
+    # reset the odd variable to that node
+    odd = odd.next_node
+
+    # change even's next_node property to point to the node after
+    # the new odd node, i.e., the next even node
+    even.next_node = odd.next_node
+
+    # reset the even variable to that node
+    even = even.next_node
+  end
+
+  # change the next_node property of the last odd node to point to the first even node
+  odd.next_node = even_head
   end
 end
